d45cda4e5478a24b07b3e8282005a2908d416600
Balintttt <88497562+Balintttt@users.noreply.github.com>
2023-07-14 12:51:54 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,6 @@ Config = {}
 Config.props = {
     ["w_me_bat"] = GetHashKey("WEAPON_BAT"),
     ["w_me_machette"] = GetHashKey("WEAPON_MACHETE"),
-    ["prop_ld_w_me_machette"] = GetHashKey("WEAPON_MACHETE"),
     ["w_ar_carbinerifle"] = GetHashKey("WEAPON_CARBINERIFLE"),
     ["w_ar_carbineriflemk2"] = GetHashKey("WEAPON_CARBINERIFLE_MK2"),
     ["w_ar_assaultrifle"] = GetHashKey("WEAPON_ASSAULTRIFLE"),
